552e74fb6b2f924f3e1f4781a4afeee161a71664
nancyma713 <nancyma713@outlook.com>
2020-05-07 00:19:47 -0400

    first commit

---
+++ stack_queue_interview_problems/lib/3_stack_queue.js
@@ -0,0 +1,127 @@
+// ============================================================================
+// Interview Problem: StackQueue
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Implement your preferred Stack implementation, including the methods:
+//
+//   - Push 
+//   - Pop 
+//   - Size
+//
+// Then, implement a Queue by instantiating two Stack instances for storage.
+//
+// The StackQueue implementation should include the following methods:
+//
+//   - Enqueue
+//   - Dequeue
+//   - Size
+//
+// -----------
+// Let's code!
+// -----------
+
+class Node {
+    // TODO: Implement the Node class!
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    // TODO: Implement the Stack class!
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    push(stackNode) {
+        if (stackNode.next) {
+            stackNode.next = null;
+        }
+
+        if (!this.top) {
+            this.top = stackNode;
+            this.bottom = stackNode;
+        } else {
+            const temp = this.top;
+            this.top = stackNode;
+            this.top.next = temp;
+        }
+        return ++this.length;
+    }
+
+    pop() {
+        if (!this.top) return null;
+
+        const temp = this.top;
+        if (this.top === this.bottom) {
+            this.top = null;
+            this.bottom = null;
+        } else {
+            this.top = this.top.next;
+        }
+        this.length--;
+        return temp;
+    }
+
+    size() {
+        return this.length;
+    }
+}
+
+class StackQueue {
+    // TODO: Implement the StackQueue class!
+    constructor() {
+        this.inStack = new Stack();
+        this.outStack = new Stack();
+        this.front = null;
+        this.back = null;
+    }
+
+    enqueue(val) {
+        const newNode = new Node(val);
+        if (!this.front) {
+            this.front = newNode;
+            this.back = newNode;
+        } else {
+            this.back.next = newNode;
+            this.back = newNode;
+        }
+
+        this.inStack.push(new Node(newNode.value));
+        return this.size();
+    }
+
+    dequeue() {
+        if (!this.front) {
+            return null;
+        } else if (this.size() === 1) {
+            this.front = null;
+            this.back = null;
+        } else {
+            this.front = this.front.next;
+        }
+        
+        if (this.outStack.size() === 0) {
+            while (this.inStack.size() > 0) {
+                this.outStack.push(this.inStack.pop());
+            }
+        }
+        let node = this.outStack.pop();
+        return node;
+    }
+
+    size() {
+        return this.inStack.size() + this.outStack.size();
+    }
+};
+
+exports.Node = Node;
+exports.Stack = Stack;
+exports.StackQueue = StackQueue;
