5451cddc623332bf02800529f49869bfe56478ae
nancyma713 <nancyma713@outlook.com>
2020-05-08 10:17:54 -0400

    leetcode problems

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -1,7 +1,27 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
+// Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+// For this problem, a height - balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
 
 function sortedArrayToBST(nums) {
+    if (!nums.length) return null;
 
+    let mid = Math.floor(nums.length / 2);
+    let root = new TreeNode(nums[mid]);
+    let left = sortedArrayToBST(nums.slice(0, mid));
+    let right = sortedArrayToBST(nums.slice(mid + 1));
+    root.left = left;
+    root.right = right;
+
+    return root;
 }
--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -2,6 +2,24 @@
 //  https://leetcode.com/problems/balanced-binary-tree/
 
 
-function isBalanced(root) {
+// Given a binary tree, determine if it is height - balanced.
 
+// For this problem, a height - balanced binary tree is defined as:
+
+// a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+function getHeight(root) {
+    if (!root) return -1;
+
+    let left = getHeight(root.left);
+    let right = getHeight(root.right);
+    return 1 + Math.max(left, right);
+}
+
+function isBalanced(root) {
+    if (!root) return true;
+
+    let balanced = Math.abs(getHeight(root.left) - getHeight(root.right) < 2);
+
+    return balanced && isBalanced(root.left) && isBalanced(root.right);
 }
