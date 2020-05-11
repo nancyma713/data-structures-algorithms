167d12fbffc62b10fb90576ad15eb79ef072ca50
nancyma713 <nancyma713@outlook.com>
2020-05-10 23:51:23 -0400

    finish num regions

--- graph_project/lib/num_regions.js
+++ graph_project/lib/num_regions.js
@@ -1,5 +1,21 @@
 function numRegions(graph) {
+    let visited = new Set();
+    let count = 0;
 
+    for (let node in graph) {
+        if (depthSearch(graph, node, visited)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+function depthSearch(graph, node, visited) {
+    if (visited.has(node)) return false;
+    visited.add(node);
+    graph[node].forEach(neighbor => depthSearch(graph, neighbor, visited));
+    return true;
 }
 
 module.exports = {
