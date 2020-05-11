8c3eee3ea6736ed84ccc6864204503d833c7c659
nancyma713 <nancyma713@outlook.com>
2020-05-10 23:44:22 -0400

    graph project

---
+++ graph_project/lib/max_value.js
@@ -0,0 +1,12 @@
+function maxValue(node, visited=new Set()) {
+    if (visited.has(node)) return null;
+    visited.add(node);
+
+    let neighborMax = node.neighbors.map(neighbor => maxValue(neighbor, visited));
+    return Math.max(node.val, ...neighborMax);
+
+}
+
+module.exports = {
+    maxValue
+};
