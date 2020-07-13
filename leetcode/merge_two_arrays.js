b47c0239b4d2ed5aea46717358c1c993c0c7a2e2
nancyma713 <nancyma713@outlook.com>
2020-07-12 23:31:19 -0400

    merge

--- leetcode/merge_two_arrays.js
+++ leetcode/merge_two_arrays.js
@@ -17,6 +17,6 @@ const merge = function (nums1, m, nums2, n) {
     return nums1;
 };
 
-console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); // [1,2,2,3,5,6]
-console.log(merge([0], 0, [2, 5, 6], 3)); // [2,5,6]
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); // [1, 2, 2, 3, 5, 6]
+console.log(merge([0], 0, [2, 5, 6], 3)); // [2, 5, 6]
 console.log(merge([0], 0, [1], 1)); // [1]
