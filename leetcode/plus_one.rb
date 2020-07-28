88f253daaf1c9258051990b81b7fb9900e6b5549
nancyma713 <nancyma713@outlook.com>
2020-07-28 18:20:45 -0400

    plus one

---
+++ leetcode/plus_one.rb
@@ -0,0 +1,18 @@
+def plus_one(digits)
+    carry = 1
+    (1..digits.length).each do |i|
+        digits[-i] += carry
+        if digits[-i] == 10
+            digits[-i] = 0
+        else
+            carry = 0
+            break
+        end
+    end
+    digits.unshift(1) if carry == 1
+    digits
+end
+
+p plus_one([1,2,3]) #=> [1,2,4]
+p plus_one([4,3,2,1]) #=> [4,3,2,2]
+p plus_one([5,3,2,9]) #=> [5,3,3,0]
