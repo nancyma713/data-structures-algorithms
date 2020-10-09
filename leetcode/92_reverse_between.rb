287395e2235e078fabc731b6a98b2bcff1b15982
nancyma713 <nancyma713@outlook.com>
2020-10-08 23:43:07 -0400

    reverse between

---
+++ leetcode/92_reverse_between.rb
@@ -0,0 +1,33 @@
+def reverse_between(head, m, n)
+    return nil if !head
+    
+    curr = head
+    prev = nil
+    
+    while m > 1
+        prev = curr
+        curr = curr.next
+        m -= 1
+        n -= 1
+    end
+    
+    conn = prev
+    tail = curr
+    third = nil
+    
+    while n > 0
+        third = curr.next
+        curr.next = prev
+        prev = curr
+        curr = third
+        n -= 1
+    end
+    
+    if conn
+        conn.next = prev
+    else
+        head = prev
+    end
+    tail.next = curr
+    head
+end
