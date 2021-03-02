5130ce201f19a90af591f83192d1069351899fc2
nancyma713 <nancyma713@outlook.com>
2021-03-01 22:52:12 -0500

    update to spiral matrix

--- leetcode/54_spiral_matrix.rb
+++ leetcode/54_spiral_matrix.rb
@@ -26,3 +26,43 @@ def spiral_order(matrix)
     
     res
 end
+
+# Space optimal solution
+
+# def spiral_order(matrix)
+#     res = []
+#     return res if matrix.length == 0
+    
+#     r1 = 0
+#     r2 = matrix.length - 1
+#     c1 = 0
+#     c2 = matrix[0].length - 1
+    
+#     while r1 <= r2 && c1 <= c2
+#         (c1..c2).each do |c|
+#             res << matrix[r1][c] 
+#         end
+#         (r1 + 1..r2).each do |r|
+#             res << matrix[r][c2]
+#         end
+        
+#         if r1 < r2 && c1 < c2
+#             c = c2 - 1
+#             while c > c1
+#                 res << matrix[r2][c]
+#                 c -= 1 
+#             end
+#             r = r2
+#             while r > r1
+#                 res << matrix[r][c1]
+#                 r -= 1
+#             end
+#         end
+#         r1 += 1
+#         r2 -= 1
+#         c1 += 1
+#         c2 -= 1
+#     end
+    
+#     res
+# end
